a0bf1be87afb5ae58b1cd08c94b31397f7510ee1
NatalyaBortsova <natanatka31@gmail.com>
2022-07-11 10:57:17 +0300

    slider

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,4 +1,8 @@
 import "./Featured.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 import BoraBora from "../../assets/destination/borabora.jpg";
 import Guernsey from "../../assets/destination/guernsey.jpg";
 import Texel from "../../assets/destination/texel.jpg";
@@ -11,74 +15,82 @@ const Featured = () => {
     <section className="featured">
       <div className="container">
         <h2 className="featured__title title">Destinations we love</h2>
-        <ul className="featured__list destination">
-          <li className="featured__item destination__item">
+        <Swiper
+          className="featured__list destination"
+          wrapperTag="ul"
+          slidesPerView={4}
+          spaceBetween={10}
+          loop={true}
+          navigation={true}
+          modules={[Navigation]}
+        >
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={BoraBora} alt="hotels" width={300} height={300} />
+                <img src={BoraBora} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Bora-Bora</span>
             </a>
             <span className="destination__number">64 properties</span>
-          </li>
-          <li className="featured__item destination__item">
+          </SwiperSlide>
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={Guernsey} alt="hotels" width={300} height={300} />
+                <img src={Guernsey} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Guernsey</span>
             </a>
             <span className="destination__number">61 properties</span>
-          </li>
-          <li className="featured__item destination__item">
+          </SwiperSlide>
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={Texel} alt="hotels" width={300} height={300} />
+                <img src={Texel} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Texel</span>
             </a>
             <span className="destination__number">411 properties</span>
-          </li>
-          <li className="featured__item destination__item">
+          </SwiperSlide>
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={Zanzibar} alt="hotels" width={300} height={300} />
+                <img src={Zanzibar} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Zanzibar</span>
             </a>
             <span className="destination__number">838 properties</span>
-          </li>
-          <li className="featured__item destination__item">
+          </SwiperSlide>
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={Bali} alt="hotels" width={300} height={300} />
+                <img src={Bali} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Bali</span>
             </a>
             <span className="destination__number">12,690 properties</span>
-          </li>
-          <li className="featured__item destination__item">
+          </SwiperSlide>
+          <SwiperSlide tag="li" className="featured__item destination__item">
             <div className="destination__image">
               <a href="#" className="destination__image-link">
-                <img src={Tenerife} alt="hotels" width={300} height={300} />
+                <img src={Tenerife} alt="hotels" width={300} height={250} />
               </a>
             </div>
             <a href="#" className="destination__link">
               <span className="destination__title">Tenerife</span>
             </a>
             <span className="destination__number">9,610 properties</span>
-          </li>
-        </ul>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
